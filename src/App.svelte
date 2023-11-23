33d434c46d5d77a04d3af5d2d293f414d974e2f4
Aditya Kumar Dubey <adityush007@gmail.com>
2023-11-23 19:13:55 +0530

    Added Frontend

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Navbar from "./lib/Navbar.svelte";
+  import UploadArea from "./lib/UploadArea.svelte";
+</script>
+
+<body>
+  <Navbar />
+  <main class="container">
+    <UploadArea />
+  </main>
+</body>
